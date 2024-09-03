704c38ffb0a0049908de7db34b0469f22413ba9c
Lucas Guidotti <lucasguidoti.lg@gmail.com>
2024-09-03 14:01:16 -0300

    segundo commit

--- gerenciadorNF.c
+++ gerenciadorNF.c
@@ -18,7 +18,7 @@ void salvarNotasFiscais(NotaFiscal notas[], int contador, const char *nomeArquiv
 void buscarNotaFiscal(NotaFiscal notas[], int contador);
 void buscarNotaFiscal(NotaFiscal notas[], int contador);
 
-// Funï¿½ï¿½o principal com o menu de opï¿½ï¿½es
+// Função principal com o menu de opções
 int main() {
 	setlocale(LC_ALL, "Portuguese");
     NotaFiscal notas[100];
@@ -35,7 +35,7 @@ int main() {
         printf("			2. LISTAR NOTAS FISCAIS\n");
         printf("			3. BUSCAR NOTAS FISCAIS\n"); 
         printf("			4. SAIR\n\n");
-        printf("			Escolha uma opï¿½ï¿½o: ");
+        printf("			Escolha uma opção: ");
         scanf("%d", &opcao);
 
         switch(opcao) {
@@ -54,20 +54,20 @@ int main() {
                 printf("\n\n			SAINDO...\n");
                 break;
             default:
-                printf("\nOpï¿½ï¿½o invï¿½lida! Tente novamente.\n");
+                printf("\nOpção inválida! Tente novamente.\n");
         }
     } while(opcao != 4);
 
     return 0;
 }
 
-// Funï¿½ï¿½o para adicionar uma nova nota fiscal
+// Função para adicionar uma nova nota fiscal
 void adicionarNotaFiscal(NotaFiscal notas[], int *contador) {
     NotaFiscal novaNota;
     novaNota.id = *contador + 1; 			//Incrementa o ID
 
-    printf("\nDigite o nï¿½mero da nota fiscal: ");
-    scanf("%d", &novaNota.numeroNota); 		// Solicita o nï¿½mero da nota fiscal
+    printf("\nDigite o número da nota fiscal: ");
+    scanf("%d", &novaNota.numeroNota); 		// Solicita o número da nota fiscal
 
     printf("Digite o nome do fornecedor: ");
     scanf(" %[^\n]", novaNota.fornecedor);  // Solicita o nome do forncedor 
@@ -76,28 +76,28 @@ void adicionarNotaFiscal(NotaFiscal notas[], int *contador) {
     scanf("%f", &novaNota.valor);  			// Solicita o valor da nota fiscal
 
     printf("Digite o caminho para o PDF da nota fiscal: ");
-    scanf(" %[^\n]", novaNota.caminhoPDF);  // Solicita o caminho em que estï¿½ armazenado o PDF da nota fiscal
+    scanf(" %[^\n]", novaNota.caminhoPDF);  // Solicita o caminho em que está armazenado o PDF da nota fiscal
 
     notas[*contador] = novaNota; 			// Insere a nota fiscal na lista
-    (*contador)++;							// Contador recebe +1 como ï¿½ ponteiro afeta diretamente a variï¿½vel original
+    (*contador)++;							// Contador recebe +1 como ï¿½ ponteiro afeta diretamente a variável original
 
     printf("\nNota fiscal adicionada com sucesso!\n");
 }
 
-// Funï¿½ï¿½o para listar todas as notas fiscais
+// Função para listar todas as notas fiscais
 void listarNotasFiscais(NotaFiscal notas[], int contador) { //Nesse caso o contador serve para informar a quantidade de notas fiscais armazenadas no array 'notas'
 	int i;
 	printf("		      ------------------------");
     printf("\n		  >>>| Lista de Notas Fiscais |<<<\n");
     printf("		      ------------------------\n\n");
     for (i = 0; i < contador; i++) {						// For para garantir que sejam impressas todas as notas fiscais armazenadas no array 'notas' em senquï¿½ncia
-        printf("ID: %d | Nï¿½mero: %d | Fornecedor: %s | Valor: %.2f | PDF: %s\n", // Imprime todos os dados jï¿½ cadastrados e armazenados no array 'notas'
+        printf("ID: %d | Número: %d | Fornecedor: %s | Valor: %.2f | PDF: %s\n", // Imprime todos os dados jï¿½ cadastrados e armazenados no array 'notas'
                notas[i].id, notas[i].numeroNota, notas[i].fornecedor, notas[i].valor, notas[i].caminhoPDF);
     }
     printf("\n");
 }
 
-// Funï¿½ï¿½o para salvar as notas fiscais em um arquivo de texto
+// Função para salvar as notas fiscais em um arquivo de texto
 void salvarNotasFiscais(NotaFiscal notas[], int contador, const char *nomeArquivo) {
     FILE *arquivo = fopen(nomeArquivo, "w"); // Utilizado para abrir um arquivo 'fopen'. 'w' indica que o arquivo serï¿½ aberto para escrita (write)
     int i;
@@ -130,7 +130,7 @@ void carregarNotasFiscais(NotaFiscal notas[], int *contador, const char *nomeArq
     fclose(arquivo);
 }
 
-// Funï¿½ï¿½o para buscar uma nota fiscal por ID ou nï¿½mero da nota
+// Função para buscar uma nota fiscal por ID ou número da nota
 void buscarNotaFiscal(NotaFiscal notas[], int contador) {
     int opcao, idBusca, numeroBusca,i;
     int encontrado = 0;
@@ -139,19 +139,19 @@ void buscarNotaFiscal(NotaFiscal notas[], int contador) {
     printf("\n		  >>>| Buscar Notas Fiscais |<<<\n");
     printf("		      ------------------------\n\n");
     printf("			1. BUSCAR POR ID\n");
-    printf("			2. BUSCAR POR Nï¿½MERO DA NOTA FISCAL\n");
-    printf("			ESCOLHA UMA OPï¿½ï¿½O: ");
+    printf("			2. BUSCAR POR NÚMERO DA NOTA FISCAL\n");
+    printf("			ESCOLHA UMA OPÇÃO: ");
     scanf("%d", &opcao);
     
     while (opcao != 1 && opcao != 2 && opcao != 3){
     	printf("		      ------------------------");
     	printf("\n		  >>>| Buscar Notas Fiscais |<<<\n");
     	printf("		      ------------------------\n\n");
-    	printf("			OPï¿½ï¿½O INVï¿½LIDA!\n");
+    	printf("			OPÇÃO INVÁLIDA!\n");
     	printf("			1. BUSCAR POR ID\n");
-    	printf("			2. BUSCAR POR Nï¿½MERO DA NOTA FISCAL\n");
+    	printf("			2. BUSCAR POR NÚMERO DA NOTA FISCAL\n");
     	printf("			3. SAIR PARA O MENU PRINCIPAL\n");
-    	printf("			ESCOLHA UMA OPï¿½ï¿½O: ");
+    	printf("			ESCOLHA UMA OPÇÃO: ");
     	scanf("%d", &opcao);
 	}
 
@@ -162,7 +162,7 @@ void buscarNotaFiscal(NotaFiscal notas[], int contador) {
 
             for (i = 0; i < contador; i++) {
                 if (notas[i].id == idBusca) {
-                    printf("ID: %d | Nï¿½mero: %d | Fornecedor: %s | Valor: %.2f | PDF: %s\n",
+                    printf("ID: %d | Número: %d | Fornecedor: %s | Valor: %.2f | PDF: %s\n",
                            notas[i].id, notas[i].numeroNota, notas[i].fornecedor, notas[i].valor, notas[i].caminhoPDF);
                     encontrado = 1;
                     break;
@@ -171,12 +171,12 @@ void buscarNotaFiscal(NotaFiscal notas[], int contador) {
             break;
 
         case 2:
-            printf("			DIGITE O Nï¿½MERO DA NOTA FISCAL: ");
+            printf("			DIGITE O NÚMERO DA NOTA FISCAL: ");
             scanf("%d", &numeroBusca);
 
             for (i = 0; i < contador; i++) {
                 if (notas[i].numeroNota == numeroBusca) {
-                    printf("ID: %d | Nï¿½mero: %d | Fornecedor: %s | Valor: %.2f | PDF: %s\n",
+                    printf("ID: %d | Número: %d | Fornecedor: %s | Valor: %.2f | PDF: %s\n",
                            notas[i].id, notas[i].numeroNota, notas[i].fornecedor, notas[i].valor, notas[i].caminhoPDF);
                     encontrado = 1;
                     break;
@@ -189,15 +189,12 @@ void buscarNotaFiscal(NotaFiscal notas[], int contador) {
             break;
             
         default:
-            printf("			OPï¿½ï¿½O INVï¿½LIDA!\n");
+            printf("			OPÇÃO INVÁLIDA!\n");
         	return;    
     }
 
     if (!encontrado) {
     	printf("\n");
-        printf("		!!!	NOTA FISCAL Nï¿½O ENCONTRADA.  !!!\n");
+        printf("		!!!	NOTA FISCAL NÃO ENCONTRADA.  !!!\n");
     }
 }
-
-
-
